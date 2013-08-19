c8193e283c944a883c5925742268cafccdd53503
Bakenokawa <mods.bakenokawa@gmail.com>
2013-08-19 16:08:27 -0500

    Added Framework for main mod class. Added Reference class to hold static information for quicker updates. Both exhist in new packages:      bakenokawa.FL_IS, and bakenokawa.FL_IS.lib. Added mcmod.info to allow for cleaner mod info display under some forge nicety.

---
+++ fl_is_commons/bakenokawa/FL_IS/lib/Reference.java
@@ -0,0 +1,8 @@
+package bakenokawa.FL_IS.lib;
+
+public class Reference
+{
+    public static final String MOD_ID = "FL_IS";
+    public static final String MOD_NAME = "Bakenokawa's Floating Islands";
+    public static final String VERSION = "0.0.1";
+}
